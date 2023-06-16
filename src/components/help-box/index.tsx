03bcfd4b18f21ef47f9032476b2bea0d49ce3118
roq-ai <>
2023-06-16 13:36:39 +0000

    Initial commit

---
+++ src/components/help-box/index.tsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import {
+  Box,
+  IconButton,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverHeader,
+  PopoverBody,
+  Text,
+  UnorderedList,
+  ListItem,
+  Link,
+} from '@chakra-ui/react';
+import { FiInfo } from 'react-icons/fi';
+import { useSession } from '@roq/nextjs';
+
+export const HelpBox: React.FC = () => {
+  const ownerRoles = ['Owner'];
+  const roles = ['Guest', 'Owner', 'Admin', 'Employee', 'HR Manager'];
+  const applicationName = `HR Information System`;
+  const tenantName = `Company`;
+  const githubUrl = process.env.NEXT_PUBLIC_GITHUB_URL;
+  const userStories = `1. As an Owner, I want to create a Company profile so that I can manage my organization's HR information.
+
+2. As an Owner, I want to invite Admins, HR Managers, and Employees to join the Company so that they can access and manage relevant information.
+
+3. As an Admin, I want to add, edit, and delete Employee profiles so that I can manage their personal and employment information.
+
+4. As an HR Manager, I want to add, edit, and delete Employee profiles so that I can manage their personal and employment information.
+
+5. As an Employee, I want to view and edit my personal information so that it is accurate and up-to-date.
+
+6. As an Admin or HR Manager, I want to manage Employee vacation requests by approving or denying them so that I can ensure proper staffing levels.
+
+7. As an Employee, I want to submit vacation requests so that I can take time off from work.
+
+8. As an Admin or HR Manager, I want to manage Employee payroll information so that I can ensure accurate and timely payments.
+
+9. As an Employee, I want to view my payroll information so that I can verify my salary and deductions.
+
+10. As a Guest, I want to submit a job application to the Company so that I can be considered for employment.
+
+11. As an Admin or HR Manager, I want to review and manage job applications from Guests so that I can identify potential candidates for open positions.`;
+
+  const { session } = useSession();
+  if (!process.env.NEXT_PUBLIC_SHOW_BRIEFING || process.env.NEXT_PUBLIC_SHOW_BRIEFING === 'false') {
+    return null;
+  }
+  return (
+    <Box width={1} position="fixed" left="30px" bottom="20px" zIndex={3}>
+      <Popover placement="top-end">
+        <PopoverTrigger>
+          <IconButton
+            aria-label="Help Info"
+            icon={<FiInfo />}
+            bg="blue.800"
+            color="white"
+            _hover={{ bg: 'blue.800' }}
+            _active={{ bg: 'blue.800' }}
+            _focus={{ bg: 'blue.800' }}
+          />
+        </PopoverTrigger>
+        <PopoverContent w="50vw" h="70vh">
+          <PopoverArrow />
+          <PopoverCloseButton />
+          <PopoverHeader>App Briefing</PopoverHeader>
+          <PopoverBody overflowY="auto">
+            <Text mb="2">Hi there!</Text>
+            <Text mb="2">
+              Welcome to {applicationName}, your freshly generated B2B SaaS application. This in-app briefing will guide
+              you through your application.
+            </Text>
+            <Text mb="2">You can use {applicationName} with one of these roles:</Text>
+            <UnorderedList mb="2">
+              {roles.map((role) => (
+                <ListItem key={role}>{role}</ListItem>
+              ))}
+            </UnorderedList>
+            {session?.roqUserId ? (
+              <Text mb="2">You are currently logged in as a {session?.user?.roles?.join(', ')}.</Text>
+            ) : (
+              <Text mb="2">
+                Right now, you are not logged in. The best way to start your journey is by signing up as{' '}
+                {ownerRoles.join(', ')} and to create your first {tenantName}.
+              </Text>
+            )}
+            <Text mb="2">
+              {applicationName} was generated based on these user stories. Feel free to try them out yourself!
+            </Text>
+            <Box mb="2" whiteSpace="pre-wrap">
+              {userStories}
+            </Box>
+            <Text mb="2">
+              If you are happy with the results, then you can get the entire source code here:{' '}
+              <Link href={githubUrl} color="cyan.500" isExternal>
+                {githubUrl}
+              </Link>
+            </Text>
+            <Text mb="2">
+              Console Dashboard: For configuration and customization options, access our console dashboard. Your project
+              has already been created and is waiting for your input. Check your emails for the invite.
+            </Text>
+            <Text mb="2">
+              <Link href="https://console.roq.tech" color="cyan.500" isExternal>
+                ROQ Console
+              </Link>
+            </Text>
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
+    </Box>
+  );
+};
